e0458502d598ddf7badfac57ee3ec121b488d236
LackFos <LackFosisOnline@gmail.com>
2023-06-30 13:12:49 +0700

    Javascript for HomePage

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,1 +1,11 @@
-console.log('Home');
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@6/swiper-bundle.esm.browser.min.js';
+import { config } from '../config/swiper';
+
+$(document).ready(function () {
+   new Swiper('#rekomendasi', config.rekomendasi);
+   new Swiper('#populer', config.populer);
+
+   $('.genre-button-more').click(function(){
+      $('#genre-list').toggleClass('aktif');
+   });
+});
