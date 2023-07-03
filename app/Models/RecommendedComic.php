f998bd9b7f7b59f72017a2b01b03a570b852351c
LackFos <LackFosisOnline@gmail.com>
2023-07-04 00:13:42 +0700

    Define Relationship for recommended_comic

--- app/Models/RecommendedComic.php
+++ app/Models/RecommendedComic.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Comic;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RecommendedComic extends Model
 {
     use HasFactory;
+
+    public function comic()
+    {
+        return $this->belongsTo(Comic::class);
+    }
 }
