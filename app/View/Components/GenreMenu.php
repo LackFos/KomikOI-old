a2db947992acb764eb3c1c8cdf209db48f4f6dac
LackFos <LackFosisOnline@gmail.com>
2023-07-06 23:59:47 +0700

    Add new parameter to GenreMenu Component - Now GenreMenu accept hasCategory parameter - Now GenreMenu can conditionaly show category type menu

--- app/View/Components/GenreMenu.php
+++ app/View/Components/GenreMenu.php
@@ -6,17 +6,20 @@ use Closure;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection as SupportCollection;
 
 class GenreMenu extends Component
 {
     /**
-     * @param Collection $genres, List of genres will be shown.
+     * @param Collection|SupportCollection $genres, List of genres will be shown.
      * @param bool $heading, Title of GenreMenu.
+     * @param bool $hasCategory, 
      * @param bool $hasMoreButton,
      */
     public function __construct(
-        public Collection $genres,
+        public Collection|SupportCollection $genres,
         public bool $heading = false,
+        public bool $hasCategory = false,
         public bool $hasMoreButton = false
     ) {
     }
