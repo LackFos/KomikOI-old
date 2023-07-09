5314b49b303d713c63c2884773ffcd845059cb65
LackFos <LackFosisOnline@gmail.com>
2023-07-10 01:22:43 +0700

    Add Jquery to Vite Builder

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+   server: {
+      host: '127.0.0.1'
+   },
+   plugins: [
+      inject({   
+         $: 'jquery',
+         jQuery: 'jquery',
+      }),
+      laravel({
+         input: ['resources/css/index.css', 'resources/js/index.js'],
+         refresh: true,
+      }),
+   ],
 });
